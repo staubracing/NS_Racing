d412cc4449498a8c049e2ab7850cfdce2b50a143
StaubRacing <staubracing@gmail.com>
2023-05-01 09:23:39 -0500

    ADDED: dotenv package

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,40 @@
-require("@nomicfoundation/hardhat-toolbox");
+const { task } = require('hardhat/config');
+
+require ('dotenv').config();
+require ("@nomicfoundation/hardhat-toolbox");
+require ("@nomicfoundation/hardhat-chai-matchers");
+
+task ("accounts", "Prints the list of accounts", async () => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.18",
+ 
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.etherscan_API_KEY,
+    }
+}
 };
+
+
+
