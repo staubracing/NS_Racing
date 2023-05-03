32ed7bfb997b3c960d1f422b82a031b8c5e094a9
StaubRacing <staubracing@gmail.com>
2023-05-03 18:25:02 -0500

    deleted notes files, update deploy file name, added contract2 to deploy file

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+/// @ title: deploy.js
+/// @ author: Staub
+
+const hre = require("hardhat"); // import hardhat
+
+async function main() { // async function
+  const contract1 = await hre.ethers.getContractFactory("StaubToken"); // get contract factory
+  const c = await contract1.deploy(); // deploy contract
+  await c.deployed(); // wait for contract to be deployed
+  console.log("staubtoken deployed to:", c.address); // log contract address
+
+  const contract2 = await hre.ethers.getContractFactory("staubCoinMintableOwnable"); // get contract factory
+  const c2 = await contract2.deploy(); // deploy contract
+  await c2.deployed(); // wait for contract to be deployed
+  console.log("staubCoinMintableOwnable deployed to:", c2.address); // log contract address
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+
+  
